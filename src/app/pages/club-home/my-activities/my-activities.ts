640125b97bac686c44969e6d9e1a7bd9c70c7a6d
caillault jeremy <jc.caillault@outlook.fr>
2026-04-02 19:42:26 +0200

    page mes activités

--- src/app/pages/club-home/my-activities/my-activities.ts
+++ src/app/pages/club-home/my-activities/my-activities.ts
@@ -1,9 +1,35 @@
-import { Component } from '@angular/core';
+import { Component, inject, OnInit, signal, computed } from '@angular/core';
+import { ActivatedRoute, RouterLink } from '@angular/router';
+import { MyActivitiesService } from '../../../core/services/my-activities.service';
+import { MyActivity } from '../../../core/models/user-activity.model';
 
 @Component({
   selector: 'app-my-activities',
-  imports: [],
+  imports: [RouterLink],
   templateUrl: './my-activities.html',
   styleUrl: './my-activities.scss',
 })
-export class MyActivities {}
+export class MyActivities implements OnInit {
+  private readonly route = inject(ActivatedRoute);
+  private readonly myActivitiesService = inject(MyActivitiesService);
+
+  readonly loading = signal(true);
+
+  readonly clubId = computed(() => Number(this.route.snapshot.paramMap.get('id')));
+
+  readonly activities = this.myActivitiesService.myActivities;
+
+  readonly approved = computed(() =>
+    this.activities().filter(a => a.status === 'APPROVED')
+  );
+  readonly pending = computed(() =>
+    this.activities().filter(a => a.status === 'PENDING')
+  );
+
+  ngOnInit(): void {
+    this.myActivitiesService.fetchMyActivities(this.clubId()).subscribe({
+      complete: () => this.loading.set(false),
+      error: () => this.loading.set(false),
+    });
+  }
+}
